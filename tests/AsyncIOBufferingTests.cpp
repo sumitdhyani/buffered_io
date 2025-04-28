62e35edc1cf4f31330505ed25936d217ca077b52
sumitdhyani <sumitdhyani1@gmail.com>
2025-04-28 19:18:23 +0530

    UnitTests: Added the AsyncIOBurring tests in which the buffer size is < the read size

--- tests/AsyncIOBufferingTests.cpp
+++ tests/AsyncIOBufferingTests.cpp
@@ -162,6 +162,156 @@ protected:
   {}
 };
 
+TEST_F(AsyncBufferTest, SearialReads_BufferSizeLessThanEverySingleReadSize)
+{
+
+  mockInput = "10HelloWorld08ByeWorld09HaleLujah10JaiShriRam";
+  auto buffer = std::make_shared<AsyncIOReadBuffer<uint32_t>>(2);
+  std::vector<std::string> msgs;
+  uint32_t totalIOCalls = 0;
+  char *buff = new char[1024]{0};
+
+  std::function<void()> readHeader = []() {};
+
+  auto ioInterface =
+      [this, &totalIOCalls](char *out, const uint32_t &len, const ResHandler &resHandler)
+  {
+    w2.push(
+        [this, out, resHandler, len, &totalIOCalls]()
+        {
+          auto readLen = mockReader(out, len);
+          ++totalIOCalls;
+          w1.push(
+              [resHandler, readLen]()
+              {
+                resHandler(readLen);
+              });
+        });
+  };
+
+  auto onMsgRead =
+      [&](const char *msg, const uint32_t &msgLen)
+  {
+    msgs.emplace_back(msg, msgLen);
+    w1.push(readHeader);
+  };
+
+  auto onHeaderRead =
+      [&](const uint32_t &msgLen)
+  {
+    buffer->read(buff,
+                 msgLen,
+                 ioInterface,
+                 [&](const uint32_t &len)
+                 {
+                   onMsgRead(buff, len);
+                 });
+  };
+
+  readHeader =
+      [&]()
+  {
+    buff[2] = '\0';
+    buffer->read(buff,
+                 2,
+                 ioInterface,
+                 [&](const uint32_t &len)
+                 {
+                   if (!len)
+                     return;
+
+                   auto msgLen = atoi(buff);
+                   onHeaderRead(msgLen);
+                 });
+  };
+
+  w1.push(readHeader);
+
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  EXPECT_EQ(msgs.size(), 4);
+  EXPECT_EQ(msgs[0], std::string("HelloWorld"));
+  EXPECT_EQ(msgs[1], std::string("ByeWorld"));
+  EXPECT_EQ(msgs[2], std::string("HaleLujah"));
+  EXPECT_EQ(msgs[3], std::string("JaiShriRam"));
+  EXPECT_EQ(totalIOCalls, 24);
+  delete[] buff;
+}
+
+TEST_F(AsyncBufferTest, SearialReads_BufferSizeLessThanTotalReadSize)
+{
+
+  mockInput = "10HelloWorld08ByeWorld09HaleLujah10JaiShriRam";
+  auto buffer = std::make_shared<AsyncIOReadBuffer<uint32_t>>(10);
+  std::vector<std::string> msgs;
+  uint32_t totalIOCalls = 0;
+  char *buff = new char[1024]{0};
+
+  std::function<void()> readHeader = []() {};
+
+  auto ioInterface =
+      [this, &totalIOCalls](char *out, const uint32_t &len, const ResHandler &resHandler)
+  {
+    w2.push(
+        [this, out, resHandler, len, &totalIOCalls]()
+        {
+          auto readLen = mockReader(out, len);
+          ++totalIOCalls;
+          w1.push(
+              [resHandler, readLen]()
+              {
+                resHandler(readLen);
+              });
+        });
+  };
+
+  auto onMsgRead =
+      [&](const char *msg, const uint32_t &msgLen)
+  {
+    msgs.emplace_back(msg, msgLen);
+    w1.push(readHeader);
+  };
+
+  auto onHeaderRead =
+      [&](const uint32_t &msgLen)
+  {
+    buffer->read(buff,
+                 msgLen,
+                 ioInterface,
+                 [&](const uint32_t &len)
+                 {
+                   onMsgRead(buff, len);
+                 });
+  };
+
+  readHeader =
+      [&]()
+  {
+    buff[2] = '\0';
+    buffer->read(buff,
+                 2,
+                 ioInterface,
+                 [&](const uint32_t &len)
+                 {
+                   if (!len)
+                     return;
+
+                   auto msgLen = atoi(buff);
+                   onHeaderRead(msgLen);
+                 });
+  };
+
+  w1.push(readHeader);
+
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  EXPECT_EQ(msgs.size(), 4);
+  EXPECT_EQ(msgs[0], std::string("HelloWorld"));
+  EXPECT_EQ(msgs[1], std::string("ByeWorld"));
+  EXPECT_EQ(msgs[2], std::string("HaleLujah"));
+  EXPECT_EQ(msgs[3], std::string("JaiShriRam"));
+  EXPECT_EQ(totalIOCalls, 6);
+  delete[] buff;
+}
+
 TEST_F(AsyncBufferTest, SearialReads)
 {
 
